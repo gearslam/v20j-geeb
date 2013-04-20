bc38c89003ae2d64763c0afc85b89afa827a80c7
evolutionmods <jameslbissonette@gamil.com>
2013-04-20 17:06:07 -0400

    acpuclock-8064: Lowered minimum clock from 384 Mhz to 192 Mhz

--- arch/arm/mach-msm/acpuclock-8064.c
+++ arch/arm/mach-msm/acpuclock-8064.c
@@ -120,7 +120,8 @@ static struct msm_bus_scale_pdata bus_scale_data __initdata = {
 };
 
 static struct l2_level l2_freq_tbl[] __initdata = {
-	[0]  = { {  384000, PLL_8, 0, 0x00 },  1050000, 1050000, 1 },
+        [0]  = { {  192000, PLL_8, 0, 0x00 },  850000, 1050000, 1 },
+	[0]  = { {  384000, PLL_8, 0, 0x00 }, 1050000, 1050000, 1 },
 	[1]  = { {  432000, HFPLL, 2, 0x20 }, 1050000, 1050000, 2 },
 	[2]  = { {  486000, HFPLL, 2, 0x24 }, 1050000, 1050000, 2 },
 	[3]  = { {  540000, HFPLL, 2, 0x28 }, 1050000, 1050000, 2 },
@@ -229,6 +230,7 @@ static struct acpu_level acpu_freq_tbl_factory_fast[] __initdata = {
 #endif
 
 static struct acpu_level tbl_slow[] __initdata = {
+        { 1, {   192000, PLL_8, 0, 0x00 }, L2(0),   875000 },
 	{ 1, {   384000, PLL_8, 0, 0x00 }, L2(0),   975000 },
 	{ 0, {   432000, HFPLL, 2, 0x20 }, L2(6),  1000000 },
 	{ 1, {   486000, HFPLL, 2, 0x24 }, L2(6),  1000000 },
@@ -255,6 +257,7 @@ static struct acpu_level tbl_slow[] __initdata = {
 };
 
 static struct acpu_level tbl_nom[] __initdata = {
+        { 1, {   192000, PLL_8, 0, 0x00 }, L2(0),   850000 },
 	{ 1, {   384000, PLL_8, 0, 0x00 }, L2(0),   925000 },
 	{ 0, {   432000, HFPLL, 2, 0x20 }, L2(6),   950000 },
 	{ 1, {   486000, HFPLL, 2, 0x24 }, L2(6),   950000 },
@@ -281,6 +284,7 @@ static struct acpu_level tbl_nom[] __initdata = {
 };
 
 static struct acpu_level tbl_fast[] __initdata = {
+        { 1, {   192000, PLL_8, 0, 0x00 }, L2(0),   775000 },
 	{ 1, {   384000, PLL_8, 0, 0x00 }, L2(0),   875000 },
 	{ 0, {   432000, HFPLL, 2, 0x20 }, L2(6),   900000 },
 	{ 1, {   486000, HFPLL, 2, 0x24 }, L2(6),   900000 },
