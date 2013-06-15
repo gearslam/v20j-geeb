3e139b43be27d2c9f9c7b97463ced70ba723ce70
evolutionmods <jameslbissonette@gmail.com>
2013-06-15 16:08:33 -0400

    Source:Starting update to 3.4.49

--- crypto/cryptd.c
+++ crypto/cryptd.c
@@ -140,10 +140,12 @@ static void cryptd_queue_worker(struct work_struct *work)
 	/* Only handle one request at a time to avoid hogging crypto
 	 * workqueue. preempt_disable/enable is used to prevent
 	 * being preempted by cryptd_enqueue_request() */
+	local_bh_disable();
 	preempt_disable();
 	backlog = crypto_get_backlog(&cpu_queue->queue);
 	req = crypto_dequeue_request(&cpu_queue->queue);
 	preempt_enable();
+	local_bh_enable();
 
 	if (!req)
 		return;
